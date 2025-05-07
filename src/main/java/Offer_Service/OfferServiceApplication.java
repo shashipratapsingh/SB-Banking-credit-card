890e36fb9ac6824c01dc4e4c0700d4f23972feac
shashipratapsingh <itpro8577@gmail.com>
2025-05-07 17:49:22 +0530

    fisrt commit

---
+++ src/main/java/Offer_Service/OfferServiceApplication.java
@@ -0,0 +1,15 @@
+package Offer_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OfferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Offer_Service/OfferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Offer_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
