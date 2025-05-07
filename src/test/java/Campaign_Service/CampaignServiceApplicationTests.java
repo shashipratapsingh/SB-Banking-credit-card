b8a5870293be24434c526a8c27e0ae949e379d06
shashipratapsingh <itpro8577@gmail.com>
2025-05-07 17:41:38 +0530

    fisrt commit

---
+++ src/test/java/Campaign_Service/CampaignServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Campaign_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
